30c818a35d69782d53847bb5663fe414080eb732
davidy <david.yuan124@gmail.com>
2014-09-22 18:35:20 +1200

    test

---
+++ neo4j-sample-jersey-simple/src/test/java/nz/co/neo4j/sample/jersey/simple/normal/BatchIntegrationTest.java
@@ -0,0 +1,14 @@
+package nz.co.neo4j.sample.jersey.simple.normal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BatchIntegrationTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
